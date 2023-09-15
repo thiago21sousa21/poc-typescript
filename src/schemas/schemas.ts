6ecdf6279deafa5d9310b1e03fa827bbb5ba573f
THIAGO SOUSA <thiago21sousa21@gmail.com>
2023-09-15 12:48:08 -0300

    final version

--- src/schemas/schemas.ts
+++ src/schemas/schemas.ts
@@ -2,8 +2,9 @@ import Joi from "joi";
 import { Movie, IdMovie } from "../protocols/protocols";
 
 export const movieSchema = Joi.object<Movie>({
-    description: Joi.string().required(),
-    title:Joi.string()
+    title:Joi.string().required(),
+    description: Joi.string()
+
 })
 
 export const idSchema = Joi.object<IdMovie>({
